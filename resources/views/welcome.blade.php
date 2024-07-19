b99e991a63eb32569f468866f0db3cac645959b8
auroraVasconcelos <aurorabsvasconcelos@gmail.com>
2024-07-19 11:59:14 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Your Words</title>
+        <title>Publisher App</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
 Route::get('/redirect', function (Request $request) {
     $request->session()->put('state', $state = Str::random(40));
  
     $query = http_build_query([
-        'client_id' => '9c8c04b6-79c1-4333-a47a-6424c4850b37',
+        'client_id' => '9c8f0d51-e0fb-4448-8ca8-a05aadf70ecb',
         'redirect_uri' => 'http://127.0.0.1:8001/callback',
         'response_type' => 'code',
         'scope' => '',
@@ -47,8 +55,8 @@ Route::get('/callback', function (Request $request) {
  
     $response = Http::asForm()->post('http://127.0.0.1:8000/oauth/token', [
         'grant_type' => 'authorization_code',
-        'client_id' => '9c8c04b6-79c1-4333-a47a-6424c4850b37',
-        'client_secret' => 'oM7rUqtBj5F9kmLVv7hjox8qbhPVBo1bPWDmL1xl',
+        'client_id' => '9c8f0d51-e0fb-4448-8ca8-a05aadf70ecb',
+        'client_secret' => 'vMTKQnDiercz6ni2xMDQQuwzZUkpIz9Wqblfm87e',
         'redirect_uri' => 'http://127.0.0.1:8001/callback',
         'code' => $request->code,
     ]);
